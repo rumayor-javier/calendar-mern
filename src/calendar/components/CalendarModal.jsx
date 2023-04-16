76145ead2ac10b7a9376019a3d574d92b366c9dd
Javier <rumayor.javier@gmail.com>
2023-04-15 23:15:47 -0600

    finished create event form validations

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
-import { addHours } from "date-fns";
+import { useMemo, useState } from "react";
+import { addHours, differenceInSeconds } from "date-fns";
 import Modal from "react-modal";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 const customStyles = {
   content: {
@@ -20,13 +23,19 @@ Modal.setAppElement("#root");
 
 export const CalendarModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(true);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const [formValues, setFormValues] = useState({
-    title: "Event",
+    title: "",
     notes: "",
     start: new Date(),
     end: addHours(new Date(), 2),
   });
+
+  const titleClass = useMemo(() => {
+    if (!formSubmitted) return "";
+    return formValues.title.trim().length > 0 ? "" : "is-invalid";
+  }, [formValues.title, formSubmitted]);
 
   const onInputChange = ({ target }) => {
     setFormValues({
@@ -35,15 +44,33 @@ export const CalendarModal = () => {
     });
   };
 
-  const onDateChange = (ev, changing) => {
+  const onDateChange = (e, changing) => {
     setFormValues({
       ...formValues,
-      [changing]: ev,
+      [changing]: e,
     });
   };
 
   const onCloseModal = () => {
     setIsModalOpen(false);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFormSubmitted(true);
+    const dateDifference = differenceInSeconds(
+      formValues.end,
+      formValues.start
+    );
+    if (isNaN(dateDifference) || dateDifference <= 0) {
+      Swal.fire(
+        "Dates Error",
+        "The end date must be greater than the start date",
+        "error"
+      );
+      return;
+    }
+    if (formValues.title.trim().length <= 0) return;
   };
 
   return (
@@ -58,7 +85,7 @@ export const CalendarModal = () => {
       <div>
         <h1> New event </h1>
         <hr />
-        <form className="container">
+        <form className="container" onSubmit={onSubmit}>
           <div className="form-group mb-2">
             <label>Starting date and time</label>
             <DatePicker
@@ -89,7 +116,7 @@ export const CalendarModal = () => {
             <label>Title and notes</label>
             <input
               type="text"
-              className="form-control"
+              className={`form-control ${titleClass}`}
               placeholder="Event title"
               name="title"
               autoComplete="off"
@@ -116,7 +143,7 @@ export const CalendarModal = () => {
             </small>
           </div>
 
-          <button type="submit" className="btn btn-outline-primary btn-block">
+          <button type="submit" className="btn btn-success btn-block">
             <i className="far fa-save"></i>
             &nbsp;
             <span>Save</span>
